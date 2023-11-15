08385ac13708ae295cb2fbc9c074af41c6018f4a
StephanieV7 <88780186+StephanieV7@users.noreply.github.com>
2023-11-15 16:31:49 +0200

    NavBar edit

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,43 +5,15 @@ export default function NavBar() {
   {/* Text Logo - Use this if you don|t have a graphic logo
   {/* <a class="navbar-brand logo-text page-scroll" href="index.html">Aria</a> */}
   {/* Image Logo */}
-  <a className="navbar-brand logo-image" href="index.html"> */
+  <a className="navbar-brand logo-image" href="index.html"> 
     <img src="images/logo.svg" alt="alternative" />
   </a>
-  {/* Mobile Menu Toggle Button */}
-  <button
-    className="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarsExampleDefault"
-    aria-controls="navbarsExampleDefault"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <span className="navbar-toggler-awesome fas fa-bars" />
-    <span className="navbar-toggler-awesome fas fa-times" />
-  </button>
-  {/* end of mobile menu toggle button */}
+
   <div className="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item">
         <a className="nav-link page-scroll" href="#header">
           HOME <span className="sr-only">(current)</span>
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link page-scroll" href="#intro">
-          INTRO
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link page-scroll" href="#services">
-          SERVICES
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link page-scroll" href="#callMe">
-          CALL ME
         </a>
       </li>
       <li className="nav-item">
